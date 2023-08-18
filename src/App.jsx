48ca851a480ab8bc1550e764e33514c722626b8b
karadagserhat <serhatkaradagg@outlook.com>
2023-08-18 10:14:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Gallery from './Gallery';
+import SearchForm from './SearchForm';
+import ThemeToggle from './ThemeToggle';
+
+// ce42RSaOV9ysrtl9VLWxl7vQksIh7PUfEicoqtF8ScU
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
